1b17d6d83f95cc8c8169ed04ddacd64e4262cc0a
Ian Emnace <igemnace@gmail.com>
2017-01-18 23:25:10 +0800

    cd to repo directory before running install

--- scripts/configs.sh
+++ scripts/configs.sh
@@ -7,14 +7,17 @@ fi
 # zsh-config
 >&2 echo "Configuring zsh..."
 git clone https://github.com/igemnace/zsh-config.git ~/DevTools/zsh-config
+cd ~/DevTools/zsh-config
 ~/DevTools/zsh-config/install.sh
 
 # tmux-config
 >&2 echo "Configuring tmux..."
 git clone https://github.com/igemnace/tmux-config.git ~/DevTools/tmux-config
+cd ~/DevTools/tmux-config
 ~/DevTools/tmux-config/install.sh
 
 # vim-config
 >&2 echo "Configuring vim..."
 git clone https://github.com/igemnace/vim-config.git ~/DevTools/vim-config
+cd ~/DevTools/vim-config
 ~/DevTools/vim-config/install.sh
